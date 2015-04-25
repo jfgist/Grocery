316f4c80833bcf8d9404580c00378bcef7a644fb
jfgist <James.Gist@gmail.com>
2015-04-25 14:44:02 -0600

    Create Main.py

---
+++ Main.py
@@ -0,0 +1,8 @@
+import csv
+
+with open('GroceryDictionary.csv', mode='r') as infile:
+    reader = csv.reader(infile)
+    with open('GroceryDictionary.csv', mode='w') as outfile:
+        writer = csv.writer(outfile)
+        mydict = {rows[0]:rows[1] for rows in reader}
+        
