264dc10fcc4ffe357dcee1c64724c27b900b871f
jfgist <James.Gist@gmail.com>
2015-04-26 02:20:45 +0000

    ...

--- Main.py
+++ Main.py
@@ -1,10 +1,16 @@
 import csv
 
+print 'read dictionary'
 with open('GroceryDictionary.csv', mode='r') as infile:
+    print 'read 1'
     reader = csv.reader(infile)
-    with open('GroceryDictionary.csv', mode='w') as outfile:
-        writer = csv.writer(outfile)
-        mydict = {rows[0]:rows[1] for rows in reader}
+    mydict = dict(reader)
+
+##    with open('GroceryDictionary.csv', mode='w') as outfile:
+#        print 'read 2'
+#        writer = csv.writer(outfile)
+#        mydict = {rows[0]:rows[1] for rows in reader}
         
+print 'iterate'
 for key in mydict:
-    print key, 'corresponds to', mydict[key]
+    print key
