9a9409929f62392e297c39ac28e46246ade1cf81
jfgist <James.Gist@gmail.com>
2015-04-26 21:39:12 +0000

    cleanup

--- Main.py
+++ Main.py
@@ -6,18 +6,16 @@ with open('GroceryDictionary.txt', mode='r') as infile:
     print 'read 1'
     reader = csv.reader(infile)
     mydict = dict(reader)
-
-##    with open('GroceryDictionary.csv', mode='w') as outfile:
-#        print 'read 2'
-#        writer = csv.writer(outfile)
-#        mydict = {rows[0]:rows[1] for rows in reader}
         
 print 'iterate'
 for key in mydict:
     print key
     
-while 1:
-    val = sys.stdin.read(1).rstrip('\n')
-    if (val == 'q'):
-        sys.exit()
-    print(mydict[val])
+
+print 'Type "quit" to exit.'
+while (True):
+  line = raw_input('PROMPT> ')
+  if line == 'quit':
+    sys.exit()
+  print(line)
+  print(mydict[line])
